bdc8c69adf3746210787207e05bc0ce5f8b04f60
tohammad <tohammad@gmail.com>
2024-07-13 21:27:43 +0530

    solidity

---
+++ contracts/Constructor.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Demo {
+
+    uint8 number;
+    constructor() {
+        number = 7;
+    }
+    function getNumber() public view returns (uint8){
+        return number;
+    }
+    function changeNumber(uint8 num) public {
+        number = num;
+    }
+  
+}
