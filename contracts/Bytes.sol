58690bff66ccf28be63d09cd81313ba3bbdbe78d
tohammad <tohammad@gmail.com>
2024-07-14 00:01:56 +0530

    solidity

---
+++ contracts/Bytes.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Bytes {
+    // bytes are stored as an array
+    bytes1 public item1 = "H";
+    bytes2 public item2 = "HA";
+    bytes3 public item3 = "HAM";
+  
+}
