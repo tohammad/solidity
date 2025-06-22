8c963f4764dcc933ab6bf8d87b308f70ef9b2550
tohammad <tohammad@gmail.com>
2025-06-22 16:51:29 +0530

    counter

--- contracts/Advanced/Counter.sol
+++ contracts/Advanced/Counter.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: MIT
+// https://sepolia.etherscan.io/tx/0xdccad1c5adb5da7d0af6c22eddb073bd8ccc9d9441f557c0c4b280072b5fa7d6
+// https://automation.chain.link/new-time-based
 pragma solidity ^0.8.0;
 contract CounterContract {
     uint public counter = 0;
